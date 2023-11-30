731d313e1cfeecd4583636c122fe9cf89ca1f01e
Mike Leach (Wander) <mike@bamboofin.tech>
2023-11-30 15:51:22 -0600

    wip

---
+++ initialize.sh
@@ -0,0 +1,35 @@
+# ensure root access
+if [ "$(id -u)" -ne 0 ]
+  then echo "Please run this script as root (or with sudo)"
+  exit
+fi
+
+# set env vars
+set -a 
+source ./.env
+set +a
+
+# clear old data (if any)
+rm -rd ./nimbus-data
+rm -rd ./tmp
+rm -rd ./stakewise-data
+rm -rd ./geth-data
+
+mkdir ./nimbus-data
+chown $USER ./nimbus-data
+
+# initialize geth
+docker compose run -d geth --authrpc.jwtsecret /tmp/jwtsecret
+sleep 3 # wait a few seconds for geth to boot up and create the jwtsecret
+chown $USER ./tmp/jwtsecret
+
+
+if [ "$NETWORK" != "mainnet" ]
+    then docker compose run nimbus trustedNodeSync -d=data --network=$NETWORK --trusted-node-url=https://checkpoint-sync.holesky.ethpandaops.io --backfill=false
+fi
+
+# pull latest stakewise operator image
+docker pull europe-west4-docker.pkg.dev/stakewiselabs/public/v3-operator:master
+
+docker compose up
+
--- snapsync.sh
+++
@@ -1,5 +0,0 @@
-set -a 
-source ./.env
-set +a
-rm -rd ./data/db
-docker compose run nimbus trustedNodeSync -d=data --network=$NETWORK --trusted-node-url=https://checkpoint-sync.holesky.ethpandaops.io --backfill=false
