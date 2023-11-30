33ed010cb4ab02f4eb7b4aa382d29d07ab4d9931
Mike Leach (Wander) <mike@bamboofin.tech>
2023-11-30 16:51:05 -0600

    change to vault-specific env files

--- initialize.sh
+++ initialize.sh
@@ -1,12 +1,21 @@
 # ensure root access
-if [ "$(id -u)" -ne 0 ]
+if [ "$(id -u)" -ne 0 ];
   then echo "Please run this script as root (or with sudo)"
   exit
 fi
 
-# set env vars
+echo $1
+
+# check parameters make sense
+if [ "$1" != "holesky" ] && [ "$1" != "gravita" ];
+  then echo "Usage: initialize.sh VAULT
+Supported vaults: holesky, gravita"
+  exit
+fi
+
+# set env based on vault provided
 set -a 
-source ./.env
+source ./${1}.env
 set +a
 
 # clear old data (if any)
@@ -21,10 +30,10 @@ chown $USER ./nimbus-data
 # initialize geth
 docker compose run -d geth --authrpc.jwtsecret /tmp/jwtsecret
 sleep 3 # wait a few seconds for geth to boot up and create the jwtsecret
+echo "looking for jwtsecret"
 chown $USER ./tmp/jwtsecret
 
-
-if [ "$NETWORK" != "mainnet" ]
+if [ "$NETWORK" != "mainnet" ];
     then docker compose run nimbus trustedNodeSync -d=data --network=$NETWORK --trusted-node-url=https://checkpoint-sync.holesky.ethpandaops.io --backfill=false
 fi
 
