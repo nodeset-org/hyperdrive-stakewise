e4d318402f69578a31368c92428fca6754927613
Mike Leach (Wander) <mike@bamboofin.tech>
2023-12-01 00:32:19 -0600

    refine docs, change script name

--- README.md
+++ README.md
@@ -8,15 +8,27 @@ You must have docker and docker compose already installed in your target environ
 
 ## Usage
 
-To set up the environment, simply run `initialize.sh`.
-Usage: `initialize.sh VAULT [--reset|-r]`
+First, clone this repository into the target location with `git clone git@github.com:nodeset-org/stakewise-reference.git`.
+
+To set up the environment, simply run `init-node.sh`.
+Usage: `init-node.sh VAULT [--reset|-r]`
 Supported vaults: `holesky`, `gravita`
-Example: `sh initialize.sh holesky` will initialize a node for [NodeSet's test vault on Holesky](https://app.stakewise.io/vault/0x01b353abc66a65c4c0ac9c2ecf82e693ce0303bc).
+Example: `sh init-node.sh holesky` will initialize a node for [NodeSet's test vault on Holesky](https://app.stakewise.io/vault/0x01b353abc66a65c4c0ac9c2ecf82e693ce0303bc).
+
+If something goes wrong, you can use the -r flag, which deletes all the chain data and client caches. On Holesky, resyncing is quick, but don't do this on mainnet if you have any active validators!
+
+Remember to forward your ports so you can find peers! Nimbus uses 9000 and Geth uses 30303.
+
+Once you run the script, logs will be shown. You may exit this view safely with `ctrl+c` and everything will continue running. To see the logs again, use `docker compose logs -f`. To bring down the containers (e.g. for maintenance), use `docker compose down`. You can safely restart everything with the same command
+
+## Environment
+
+At a high level, the `init-node.sh` script wraps a docker compose file, `compose.yaml`. It sets up the environment, then executes `docker compose up -d`. To reset
+
+## Internal Software
 
 [`operator`](https://github.com/stakewise/v3-operator): the StakeWise v3-operator software used to generate keystores and validator deposit data
 
 [Nimbus](https://nimbus.guide/): a lightweight Ethereum Validator Client
 
 [`ethdo`](https://github.com/wealdtech/ethdo): an eth2 utility developed by Jim McDonald used to generate
-
-NOTE: Remember to forward your ports so you can find peers! Nimbus uses 9000 and Geth uses 30303.
---
+++ init-node.sh
@@ -0,0 +1,101 @@
+clean()
+{
+    echo "Cleaning up previous configuration..."
+    # clear old data (if any)
+    rm -rd ./nimbus-data
+    rm -rd ./tmp
+    rm -rd ./stakewise-data
+    rm -rd ./geth-data
+
+    mkdir ./nimbus-data
+    chown $(logname) ./nimbus-data
+}
+
+generate_jwtsecret()
+{
+    echo "Generating jwtsecret..."
+    # initialize geth
+    docker compose run -d geth --authrpc.jwtsecret /tmp/jwtsecret
+    sleep 3 # wait a few seconds for geth to boot up and create the jwtsecret
+    chown $(logname) ./tmp/jwtsecret
+}
+
+checkpoint()
+{
+    echo "Performing checkpoint sync..."
+    if [ "$NETWORK" != "mainnet" ] then
+        docker compose run nimbus trustedNodeSync -d=/home/user/data --network=$NETWORK --trusted-node-url=https://checkpoint-sync.holesky.ethpandaops.io --backfill=false
+    fi
+}
+
+setup_stakewise()
+{
+    echo "Pulling latest StakeWise operator binary..."
+    # pull latest stakewise operator image
+    docker pull europe-west4-docker.pkg.dev/stakewiselabs/public/v3-operator:master
+}
+
+# ensure root access
+if [ "$(id -u)" -ne 0 ] then
+  echo "Please run this script as root (or with sudo)"
+  exit
+fi
+
+USAGEMSG="Usage: init-node.sh VAULT [--preserve-data|-p]
+Supported vaults: holesky, gravita"
+
+# check parameters make sense
+if [ "$1" != "holesky" ] && [ "$1" != "gravita" ] then
+  echo $USAGEMSG
+  exit
+fi
+
+# set env based on vault provided
+set -a 
+source ./${1}.env
+set +a
+
+# check for preserve-data flag
+if [ "$2" != "" ] && [ "$2" != "-r" ] && [ "$2" != "--reset" ] then
+    echo $USAGEMSG
+    exit
+fi
+
+if [ "$2" = "-r" ] || [ "$2" == "--reset" ] then
+     if [ "$1" != "holesky" ] then
+        
+        # todo: check if there are any active validators before giving this warning
+        echo "DANGER: You are attempting to reset your configuration for a mainnet vault!"
+        echo "This will require you to resync the chain completely before you can begin validating again, which may take several days."
+        echo "Remember, if you're offline for too long, you may be kicked out of NodeSet!"
+        echo "Are you sure you want to continue? You must type YES to continue."
+        read answer
+
+        if [ "$answer" != "YES" ] then 
+            echo Cancelled
+            exit
+        fi
+
+        echo "THIS IS YOUR FINAL WARNING! Are you absolutely sure that you want to delete all of your data for this mainnet configuration?"
+        echo "You must type YES to continue."
+        read answer2
+
+        if [ "$answer2" != "YES" ] then 
+            echo Cancelled
+            exit
+        fi
+    fi
+    clean
+fi
+
+generate_jwtsecret
+checkpoint
+setup_stakewise
+
+echo Starting node...
+docker compose up -d
+echo "{::} Node started successfully!"
+echo
+echo "After continuing, logs will be displayed. Please inspect them to verify everything is working as expected. Once you're satisfied, you may exit safely and the containers will continue running."
+read -rsn1 -p "Press any key to continue..."; echo
+docker compose logs -f
