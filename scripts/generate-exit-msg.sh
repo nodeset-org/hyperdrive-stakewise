3563163d70e36d5ff883c14ef605ca56f238c5a1
Mike Leach (Wander) <mike@bamboofin.tech>
2023-12-19 04:05:43 +0000

    wip

--- scripts/generate-exit-msg.sh
+++ scripts/generate-exit-msg.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash 
 
 echo "Generating exit messages..."
-docker compose -f "$DATA_DIR/compose.yaml" run ethdo validator exit --validator=$DATA_DIR/stakewise-data/$VAULT/keystore-m_12381_3600_0_0_0-1702688242.json --offline
+docker compose -f "$DATA_DIR/compose.yaml" run ethdo validator exit --account=$DATA_DIR/stakewise-data/$VAULT/keystore-m_12381_3600_0_0_0-1702688242.json --passphrase=$DATA_DIR/stakewise-data/$VAULT/password.txt --offline
