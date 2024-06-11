7c419b19de7676c365244c3cf4856c08df53bdff
huy <huy@nodeset.io>
2024-06-11 09:04:45 -0700

    Add PullSplit to resources

--- shared/config/resources.go
+++ shared/config/resources.go
@@ -28,6 +28,10 @@ type StakewiseResources struct {
 	// All node op rewards will live here; to claim them, call `Withdraw`.
 	// See https://docs.splits.org/core/warehouse
 	SplitWarehouse *common.Address
+
+	// The address of the PullSplit contract used to manage recipients/allocations and distributions
+	// See https://docs.splits.org/core/split-v2
+	PullSplit *common.Address
 }
 
 // Creates a new resource collection for the given network
@@ -39,6 +43,7 @@ func newStakewiseResources(network config.Network) *StakewiseResources {
 		Vault:            nil,
 		FeeRecipient:     nil,
 		SplitWarehouse:   nil,
+		PullSplit:        nil,
 	}
 
 	// Holesky
@@ -48,6 +53,7 @@ func newStakewiseResources(network config.Network) *StakewiseResources {
 		Vault:            config.HexToAddressPtr("0x646F5285D195e08E309cF9A5aDFDF68D6Fcc51C4"),
 		FeeRecipient:     config.HexToAddressPtr("0xc98F25BcAA6B812a07460f18da77AF8385be7b56"),
 		SplitWarehouse:   config.HexToAddressPtr("0x8fb66F38cF86A3d5e8768f8F1754A24A6c661Fb8"),
+		PullSplit:        config.HexToAddressPtr("0xAefad0Baa37e1BAF14404bcc2c5E91e4B41c929B"),
 	}
 
 	// Holesky Dev
@@ -57,6 +63,7 @@ func newStakewiseResources(network config.Network) *StakewiseResources {
 		Vault:            config.HexToAddressPtr("0xf8763855473ce978232bBa37ef90fcFc8aAE10d1"),
 		FeeRecipient:     config.HexToAddressPtr("0xc98F25BcAA6B812a07460f18da77AF8385be7b56"),
 		SplitWarehouse:   config.HexToAddressPtr("0x8fb66F38cF86A3d5e8768f8F1754A24A6c661Fb8"),
+		PullSplit:        config.HexToAddressPtr("0xAefad0Baa37e1BAF14404bcc2c5E91e4B41c929B"),
 	}
 	holeskyDevResources.Network = hdconfig.Network_HoleskyDev
 
