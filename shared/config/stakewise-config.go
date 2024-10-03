19d5dfe09497d90f271ede6fc0ad4304b699347a
Joe Clapis <jclapis@outlook.com>
2024-10-03 00:03:45 -0400

    Updated SW operator

--- shared/config/stakewise-config.go
+++ shared/config/stakewise-config.go
@@ -13,7 +13,7 @@ import (
 const (
 	// Tags
 	daemonTag   string = "nodeset/hyperdrive-stakewise:v" + shared.StakewiseVersion
-	operatorTag string = "europe-west4-docker.pkg.dev/stakewiselabs/public/v3-operator:v2.1.2"
+	operatorTag string = "europe-west4-docker.pkg.dev/stakewiselabs/public/v3-operator:v2.1.3"
 )
 
 // Configuration for Stakewise
