62b8b0dd55ec0e4137e3b4379d85a61913b3b62d
huy <huy@nodeset.io>
2025-01-07 16:43:40 -0800

    rest of functions

--- adapter/config/upload-deposit-data.go
+++ adapter/config/upload-deposit-data.go
@@ -1,30 +1,33 @@
 package config
 
 import (
+	"fmt"
+
+	"github.com/nodeset-org/hyperdrive-stakewise/adapter/utils"
+	swclient "github.com/nodeset-org/hyperdrive-stakewise/client"
 	"github.com/urfave/cli/v2"
 )
 
 func uploadDepositData(c *cli.Context) error {
-	return nil
-	// // Get the client
-	// hd, err := client.NewHyperdriveClientFromCtx(c)
-	// if err != nil {
-	// 	return err
-	// }
-	// sw, err := client.NewStakewiseClientFromCtx(c, hd)
-	// if err != nil {
-	// 	return err
-	// }
-	// cfg, _, err := hd.LoadConfig()
-	// if err != nil {
-	// 	return fmt.Errorf("error loading Hyperdrive config: %w", err)
-	// }
-	// if !cfg.StakeWise.Enabled.Value {
-	// 	fmt.Println("The StakeWise module is not enabled in your Hyperdrive configuration.")
-	// 	return nil
-	// }
+	// Get the client
+	hd, err := swclient.NewHyperdriveClientFromCtx(c)
+	if err != nil {
+		return err
+	}
+	sw, err := swclient.NewStakewiseClientFromCtx(c, hd)
+	if err != nil {
+		return err
+	}
+	cfg, _, err := hd.LoadConfig()
+	if err != nil {
+		return fmt.Errorf("error loading Hyperdrive config: %w", err)
+	}
+	if !cfg.StakeWise.Enabled.Value {
+		fmt.Println("The StakeWise module is not enabled in your Hyperdrive configuration.")
+		return nil
+	}
 
-	// // Upload to the server
-	// _, err = swcmdutils.UploadDepositData(c, hd, sw)
-	// return err
+	// Upload to the server
+	_, err = utils.UploadDepositData(c, hd, sw)
+	return err
 }
